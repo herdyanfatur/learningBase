a87fb51f1140118bf34aa1444081f19218819a92
herdyanfatur <herdyanfatur@gmail.com>
2025-12-11 10:13:00 +0700

    add the interface function for input integer

--- programmingFundamental.js
+++ programmingFundamental.js
@@ -1,7 +1,19 @@
-function starTriangle(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log("*".repeat(i));
-  }
-}
+const readline = require("readline");
 
-starTriangle(5);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("=== Masukan total baris bintang yang ingin anda tampilkan ===");
+rl.question("Total : ", function(total) {
+  const n = parseInt(total);    
+        function starTriangle(n) {
+            for (let i = 1; i <= n; i++) {
+                console.log("*".repeat(i));
+            }
+        }
+        starTriangle(n);
+        rl.close();
+    });
+
